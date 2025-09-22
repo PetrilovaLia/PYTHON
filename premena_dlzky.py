5a789c4b940f8f8ca944b2ee4a612506e90b1d90
PetrilovaLia <natalia.petrilova@gmail.com>
2025-09-22 22:24:34 +0200

    Create premena_dlzky.py

---
+++ premena_dlzky.py
@@ -0,0 +1,14 @@
+# Program na premenu jednotiek dĺžky
+
+# Vstup od používateľa
+km = float(input("Zadaj vzdialenosť v kilometroch: "))
+
+# Prepočty
+m = km * 1000
+cm = m * 100
+mm = cm * 10
+
+# Výstup
+print("Vzdialenosť v metroch:", m)
+print("Vzdialenosť v centimetroch:", cm)
+print("Vzdialenosť v milimetroch:", mm)
