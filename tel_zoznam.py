5f91e019b68e668a337e0289fbacacfe0f944aaf
PetrilovaLia <natalia.petrilova@gmail.com>
2025-09-24 11:11:54 +0200

    Update tel_zoznam.py

--- tel_zoznam.py
+++ tel_zoznam.py
@@ -16,8 +16,17 @@ while True:
         kontakty[meno] = cislo
         print("Kontakt bol pridaný.")
 
-#Program sa pokúsi nájsť číslo pomocou kontakty.get(meno)
+
     elif volba == "2":
+        if kontakty:
+            print("Všetky kontakty:")
+            for meno, cislo in kontakty.items():
+                print(f"{meno}: {cislo}")
+        else:
+            print("Zoznam kontaktov je prázdny.")
+            
+#Program sa pokúsi nájsť číslo pomocou kontakty.get(meno)
+    elif volba == "3":
         meno = input("Zadajte meno kontaktu: ")
         cislo = kontakty.get(meno)
         if cislo:
@@ -25,17 +34,19 @@ while True:
         else:
             print("Kontakt sa nenašiel.")
 
-    elif volba == "3":
-        if kontakty:
-            print("Všetky kontakty:")
-            for meno, cislo in kontakty.items():
-                print(f"{meno}: {cislo}")
-        else:
-            print("Zoznam kontaktov je prázdny.")
-
+    
     elif volba == "4":
         print("Ukončujem program...")
         break
 
     else:
         print("Neplatná voľba. Skúste znova.")
+
+meno = 'Alex'
+vek = 18
+
+print(meno + ' : ' + str(vek))
+print(meno, ':', vek)
+print(f'{meno} : {vek}')
+print('%s : %s ' % (meno, vek))
+print('{} : {}' .format(meno,vek))
